3927fe1a4f7322d93ae75bbd88f7e5376b2799aa
vinybergamo <vinybergamo@gmail.com>
2024-09-21 02:26:38 -0300

    chore: Add global types and update nest-cli.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -15,8 +14,7 @@ import { ConfigModule } from '@nestjs/config';
         '.env.local',
       ],
     }),
+    DatabaseModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
